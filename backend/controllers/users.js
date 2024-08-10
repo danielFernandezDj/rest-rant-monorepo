9b85a373a5ebfae99dd0e41ae87e83ddef21d419
“Foxshane997” <Foxshane997@gmail.com>
2024-08-10 11:58:34 -0700

    finished homework 3

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,10 +1,16 @@
 const router = require('express').Router()
 const db = require("../models")
+const bcrypt = require('bcrypt')
 
 const { User } = db
 
 router.post('/', async (req, res) => {
-    const user = await User.create(req.body)
+    let { password, ...rest} = req.body;
+    let passDigest = await bcrypt.hash(password, 10)
+    console.log("passDigest test", passDigest)
+    const user = await User.create({
+        ...rest,
+        passwordDigest: passDigest})
     res.json(user)
 })
 

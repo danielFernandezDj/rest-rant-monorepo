bc263576b49e3806bfae98165ff95353f7b385c6
“Foxshane997” <Foxshane997@gmail.com>
2024-08-10 13:22:51 -0700

    finished hw 4 and all is working

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,26 +1,30 @@
 import { createContext, useEffect, useState } from "react";
 
+export const CurrentUser = createContext();
 
-export const CurrentUser = createContext()
-
-function CurrentUserProvider({ children }){
-
-    const [currentUser, setCurrentUser] = useState(null)
-    useEffect(() => {
-
-        const getLoggedInUser = async () => {
-            let response = await fetch('http://localhost:5000/authentication/profile')
-            let user = await response.json()
-            setCurrentUser(user)
+function CurrentUserProvider({ children }) {
+  const [currentUser, setCurrentUser] = useState(null);
+  useEffect(() => {
+    const getLoggedInUser = async () => {
+      let response = await fetch(
+        "http://localhost:5000/authentication/profile",
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem("token")}`,
+          },
         }
-        getLoggedInUser()
-    }, [])
+      );
+      let user = await response.json();
+      setCurrentUser(user);
+    };
+    getLoggedInUser();
+  }, []);
 
-    return (
-        <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-            {children}
-        </CurrentUser.Provider>
-    )
+  return (
+    <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
+      {children}
+    </CurrentUser.Provider>
+  );
 }
 
-export default CurrentUserProvider
+export default CurrentUserProvider;

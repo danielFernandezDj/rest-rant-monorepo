921564a5ccfc85e7fb9592e1ec55a691414b8ead
“Foxshane997” <Foxshane997@gmail.com>
2024-08-10 12:24:40 -0700

    finished sessions activity

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
 const { User } = db
+
 router.post('/', async (req, res) => {
     
     let user = await User.findOne({
@@ -16,4 +17,35 @@ router.post('/', async (req, res) => {
         res.json({ user })
     }
 })
+ 
+router.post('/', async (req, res) => {
+    
+    let user = await User.findOne({
+        where: { email: req.body.email }
+    })
+
+    if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
+        res.status(404).json({ 
+            message: `Could not find a user with the provided username and password` 
+        })
+    } else {
+        req.session.userId = user.userId
+        res.json({ user })                                       
+    }
+})
+
+router.get('/profile', async (req, res) => {
+    console.log(req.session.userId)
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId,                   
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
+
 module.exports = router
